d70ee8a650f3258e6d55da6ddc53a410c612d688
guptakanishka968-sys <guptakanishka968@gmail.com>
2026-03-12 01:19:36 +0530

    Refactor app.py to improve structure and readability

--- resume-analyzer-website/app.py
+++ resume-analyzer-website/app.py
@@ -334,7 +334,7 @@ st.progress(ats_score / 100)
     # ---------------------------
     # Column 2: Skills display, Graphs & WordCloud
     # ---------------------------
-    with col2:
+with col2:
 
         if uploaded_file:
 
@@ -439,6 +439,7 @@ for i, record in enumerate(st.session_state.history):
     st.write("Missing Skills:", ", ".join(record["missing_skills"]) if record["missing_skills"] else "None! Great job!")
 
     st.markdown("---")
+
 
 
 
